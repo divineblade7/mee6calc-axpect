097409aeb27834136d7e8edee3f93b542e13400f
divineblade7 <32503738+divineblade7@users.noreply.github.com>
2024-01-20 14:18:24 -0800

    re-edit
    complete restart of all edits.

--- mee6calc.js
+++ mee6calc.js
@@ -1,36 +1,35 @@
-function mee6calculator()
-{
+function mee6calculator() {
     var minxp = document.getElementById("minxp").value;
     var maxxp = document.getElementById("maxxp").value;
+
     var desiredlvl = document.getElementById("desired-level").value;
     var currentlvl = document.getElementById("current-lvl").value;
     var xpinlvl = document.getElementById("current-xp").value;
-    var megapotions = document.getElementById(megapotions).value;
-    var ultrapotions = document.getElementById(ultrapotions).value;
 
-    //Magical math functions
-    
+    // Magical math functions
     var currentxp = 5/6 * currentlvl * (2 * currentlvl * currentlvl + 27 * currentlvl + 91);
     var totalxp = parseInt(currentxp) + parseInt(xpinlvl);
     var desiredxp = 5/6 * desiredlvl * (2 * desiredlvl * desiredlvl + 27 * desiredlvl + 91);
     var totaldesiredxp = parseInt(desiredxp) - parseInt(totalxp);
-    var megapotions = totaldesiredxp/30000;
-    var ultrapotions = megapotions/15;
 
-
-    //Minimum, average, and maximum messages needed to send in order to reach specified level
+    // Minimum, average, and maximum messages needed to send in order to reach specified level
     var minmsg = Math.ceil(totaldesiredxp / maxxp);
     var avg = parseInt(minxp) + parseInt(maxxp);
-    var avgxp = avg/2;
+    var avgxp = avg / 2;
     var avgmsg = Math.ceil(totaldesiredxp / avgxp);
     var maxmsg = Math.ceil(totaldesiredxp / minxp);
 
-    //Display information to user
+    // Calculate mega and ultra potions needed
+    var megaPotionsNeeded = Math.floor(totaldesiredxp / 30000);
+    var ultraPotionsNeeded = Math.floor(totaldesiredxp / 450000);
+
+    // Display information to the user
     document.getElementById("result-average").innerHTML = avgmsg;
     document.getElementById("result-xp-needed").innerHTML = totaldesiredxp;
-    document.getElementById("result-megapotions-needed").innerHTML = megapotions;
-    document.getElementById("result-ultrapotions-needed").innerHTML = ultrapotions;
     document.getElementById("result-minimum-messages-requirement").innerHTML = minmsg;
     document.getElementById("result-maximum-messages-requirement").innerHTML = maxmsg;
-  
+
+    // Display potion information
+    document.getElementById("result-mega-potions-needed").innerHTML = megaPotionsNeeded;
+    document.getElementById("result-ultra-potions-needed").innerHTML = ultraPotionsNeeded;
 }
--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-function mee6calculator()
-{
+function mee6calculator() {
     var minxp = document.getElementById("minxp").value;
     var maxxp = document.getElementById("maxxp").value;
     var desiredlvl = document.getElementById("desired-level").value;
     var currentlvl = document.getElementById("current-lvl").value;
     var current_xp = document.getElementById("current-xp").value;
-    var megapotions = document.getElementById(megapotions).value;
-    var ultrapotions = document.getElementById(ultrapotions).value;
 
-    //Magical math functions
-    
-    var currentxp = (5/6 * currentlvl * (2 * currentlvl * currentlvl + 27 * currentlvl + 91)) + current_xp;
+    // Magical math functions
+    var currentxp = (5/6 * currentlvl * (2 * currentlvl * currentlvl + 27 * currentlvl + 91)) + parseInt(current_xp);
     var desiredxp = 5/6 * desiredlvl * (2 * desiredlvl * desiredlvl + 27 * desiredlvl + 91) - currentxp;
 
-    //Minimum, average, and maximum messages needed to send in order to reach specified level
+    // Minimum, average, and maximum messages needed to send in order to reach the specified level
     var minmsg = Math.ceil(desiredxp / maxxp);
-    var avgmsg = Math.ceil(desiredxp / ((minxp + maxxp) / 2));
+    var avgmsg = Math.ceil(desiredxp / ((parseInt(minxp) + parseInt(maxxp)) / 2));
     var maxmsg = Math.ceil(desiredxp / minxp);
 
-    //Display information to user
+    // Calculate mega and ultra potions needed
+    var megaPotionsNeeded = Math.floor(desiredxp / 30000);
+    var ultraPotionsNeeded = Math.floor(desiredxp / 450000);
+
+    // Display information to the user
     document.getElementById("result-average").innerHTML = avgmsg;
     document.getElementById("result-xp-needed").innerHTML = desiredxp;
-    document.getElementById("result-mega-potions-needed").innerHTML = megapotions;
-    document.getElementById("result-ultra-potions-needed").innerHTML = ultrapotions;
+    document.getElementById("result-mega-potions-needed").innerHTML = megaPotionsNeeded;
+    document.getElementById("result-ultra-potions-needed").innerHTML = ultraPotionsNeeded;
     document.getElementById("result-minimum-messages-requirement").innerHTML = minmsg;
     document.getElementById("result-maximum-messages-requirement").innerHTML = maxmsg;
-  
 }
